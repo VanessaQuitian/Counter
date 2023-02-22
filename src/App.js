81a0716d8b0c0969a7fe8d908fbbc46c2cffbd80
Vanessa Silva <karelyssilva08122003@gmail.com>
2023-02-21 20:57:30 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {useEffect, useState } from 'react';
 
 function App() {
+
+  // primer posicion: valor inicial
+  //Segunda posición: función que va alterar variable
+  const [counter, setCounter] = useState(0);
+  const [style, setStyles] = useState('min');
+
+  const handleAddition = () => {
+    setCounter(counter+1)
+  }
+
+  const handleRest = () => {
+    setCounter(counter-1)
+  }
+
+  const handleReset = () => {
+    setCounter(0)
+  }
+
+  useEffect (()=>{
+    counter >= 10 ? setStyles('max'):setStyles('min')
+  },[counter]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2 className={style}>{counter}</h2>
+      <button onClick={handleAddition}>Sumar</button>
+      <button onClick={handleRest}>Restar</button>
+      <button onClick={handleReset}>Resetear</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
